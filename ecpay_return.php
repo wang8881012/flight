a5fd16bd9ccd160dcf63d3f6f59d2d728fbd6a2e
Jason <djskciam61217@gmail.com>
2025-07-06 23:56:50 +0800

    2025/07/06 儲存

---
+++ ecpay_return.php
@@ -0,0 +1,3 @@
+<?php
+file_put_contents('log.txt', print_r($_POST, true)); // 簡單記錄結果
+echo '付款完成！';
---
+++ js/confirm.js
@@ -0,0 +1,17 @@
+const collapseElements = document.querySelectorAll('.collapse');
+
+collapseElements.forEach(collapseEl => {
+    collapseEl.addEventListener('show.bs.collapse', function () {
+        const targetId = this.getAttribute('id');
+        const icon = document.querySelector(`i[data-toggle-icon='${targetId}']`);
+        icon.classList.remove('bi-caret-down-fill');
+        icon.classList.add('bi-caret-up-fill');
+    });
+
+    collapseEl.addEventListener('hide.bs.collapse', function () {
+        const targetId = this.getAttribute('id');
+        const icon = document.querySelector(`i[data-toggle-icon='${targetId}']`);
+        icon.classList.remove('bi-caret-up-fill');
+        icon.classList.add('bi-caret-down-fill');
+    });
+});
