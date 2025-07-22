8a1dac6e6e0006f65a76cbdaf7a2e5b916c56011
Jason <djskciam61217@gmail.com>
2025-07-22 12:41:10 +0800

    Revert "刪除arrow_button.js，與confirm.js合併"
    This reverts commit ecbc144a484c1ebe894a782034beb59539850f3e.

--- js/confirm.js
+++ js/confirm.js
@@ -80,22 +80,3 @@ function formatDate(datetime) {
 function padZero(num) {
     return num < 10 ? '0' + num : num;
 }
-
-//卡片下拉選單功能 ()
-const collapseElements = document.querySelectorAll('.collapse');
-
-collapseElements.forEach(collapseEl => {
-    collapseEl.addEventListener('show.bs.collapse', function () {
-        const targetId = this.getAttribute('id');
-        const icon = document.querySelector(`i[data-toggle-icon='${targetId}']`);
-        icon.classList.remove('bi-caret-down-fill');
-        icon.classList.add('bi-caret-up-fill');
-    });
-
-    collapseEl.addEventListener('hide.bs.collapse', function () {
-        const targetId = this.getAttribute('id');
-        const icon = document.querySelector(`i[data-toggle-icon='${targetId}']`);
-        icon.classList.remove('bi-caret-up-fill');
-        icon.classList.add('bi-caret-down-fill');
-    });
-});
