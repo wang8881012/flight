b086e6d798b8fde24b52859a539d64b73558e465
Joan <joanshen005@gmail.com>
2025-07-23 10:20:47 +0800

    change nav and footer & db connection path

--- admin/api/orders.php
+++ admin/api/orders.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['admin'])) {
   exit;
 }
 
-require_once __DIR__ . '/../inc/db.inc.php';
+require_once __DIR__ . '../../../api/inc/db.inc.php';
 
 // 取得 POST 傳來的資料
 $data = json_decode(file_get_contents("php://input"), true);
