1235fc85d51d31721296861db14565b9737ea900
Joan <joanshen005@gmail.com>
2025-08-01 14:14:35 +0800

    vue路徑修改

--- my-vue-app/src/router/index.js
+++ my-vue-app/src/router/index.js
@@ -21,7 +21,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/adminpage/'),
+  history: createWebHistory('/flight/adminpage/'),
   routes,
 });
 
--- my-vue-app/vite.config.js
+++ my-vue-app/vite.config.js
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/adminpage/',
+  base: '/flight/adminpage/',
   plugins: [vue()],
   server: {
     proxy: {
